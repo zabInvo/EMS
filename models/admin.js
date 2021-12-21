ccade48b9f0ac3f3459cdba7cba0ca84e1c13378
Zain Ali <zain.ali@invozone.com>
2021-12-21 17:12:26 +0500

    Models, Migrations , Config

---
+++ models/admin.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Admin.init(
+    {
+      name: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [5, 30],
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+    },
+    {
+      paranoid: true,
+      sequelize,
+      modelName: "Admin",
+    }
+  );
+  return Admin;
+};
